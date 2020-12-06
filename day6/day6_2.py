32e2b63790c4def2e8cfced4275f964e294a8f6b
Hannes <k.hannes.simonsson@gmail.com>
2020-12-06 18:37:01 +0100

    added solutions for day 6

---
+++ day6/day6_2.py
@@ -0,0 +1,66 @@
+'''
+--- Part Two ---
+As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
+
+You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+
+Using the same example as above:
+
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+This list represents answers from five groups:
+
+-In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+-In the second group, there is no question to which everyone answered "yes".
+-In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+-In the fourth group, everyone answered yes to only 1 question, a.
+-In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+
+In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+'''
+import re
+
+# get input and sort it as groups of answers -> [[answers, answers],[answers, answers]]
+with open('input_day6.txt') as f:
+    inputs = [re.sub('\n', ' ' , x.strip('\n')).split() for x in f.read().split('\n\n')]
+
+# check each group, handle edge case of groups with only one respondant
+answered = 0
+for group in inputs:
+    if len(group) == 1:
+        answered += len(group[0])
+        
+    else:
+        # pick one set of answers
+        answers = group.pop(0)
+        # for each answer in the set, check if it exists in any other set of answers
+        # if the asnwer doesn't exist in one set, set it to not valid and check next answer
+        for answer in answers:
+            for check in group:
+                if answer in check:
+                    valid = True
+                else:
+                    valid = False
+                    break
+            
+            # cumulative addition of valid answers
+            if valid:
+                answered += 1
+                valid = False
+
+print(answered)
